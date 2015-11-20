2d3ac4f428f7f2cd8c25e6466be21636a2856918
wangsongyan <wangsongyan@users.noreply.github.com>
2015-11-20 13:42:09 +0800

    Update download.js

--- www/download.js
+++ www/download.js
@@ -1,7 +1,6 @@
-cordova.define("cordova-plugin-download.Download", function(require, exports, module) { 
-	module.exports = {
-	    download: function (message, win, fail) {
-	        cordova.exec(win, fail, "Downloader", "download", [message]);
-	    }  
-	};
-});
+module.exports = {
+    download: function (message, win, fail) {
+        cordova.exec(win, fail, "Downloader", "download", [message]);
+    }  
+};
+
